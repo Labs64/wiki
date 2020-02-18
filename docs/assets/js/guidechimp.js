bbbf5662b4df9d0ece7223d36c533daa8819cd7b
Alexey Averikhin <alexey.averikhin@labs64.de>
2020-02-18 23:44:04 +0100

    - additional tour steps

--- docs/assets/js/guidechimp.js
+++ docs/assets/js/guidechimp.js
@@ -1,18 +1,28 @@
 var tour = [
     {
-        element: 'stay-up-to-date-with-netlicensing-platform-news',
+        element: '#stay-up-to-date-with-netlicensing-platform-news',
         title: 'NetLicensing Platform News',
         description: 'Stay informed about the latest changes to NetLicensing platform directly in the management console.',
     },
     {
-        element: 'extended-customer-data',
+        element: '#extended-customer-data',
         title: 'Extended Customer Data',
         description: 'Improve customer profiles by adding and grouping customer information using Custom Properties in the UI.',
     },
     {
-        element: 'pricing-table-licensing-model',
+        element: '#pricing-table-licensing-model',
         title: 'Pricing Table Licensing Model',
         description: 'Easily maintain Pricing Table & Membership Plans for your SaaS products in the NetLicensing UI.',
+    },
+    {
+        element: '#labs64-netlicensing-for-github-students',
+        title: 'Labs64 NetLicensing for GitHub Students',
+        description: 'Great news for all students - Labs64 NetLicensing partnered with GitHub and offers you NetLicensing Student (Basic) Plan for FREE while you are a student.',
+    },
+    {
+        element: '#vendor-usage-page',
+        title: 'Vendor Usage Page',
+        description: 'Easily verify the limits (active products, customers) and available features and licensing models within your NetLicensing vendor account.',
     },
 ];
 
