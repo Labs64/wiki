853223f43c573909e79f9128fca267eafb39a913
Alexey Averikhin <alexey.averikhin@labs64.de>
2020-02-18 23:25:05 +0100

    - add step 3

--- docs/assets/js/guidechimp.js
+++ docs/assets/js/guidechimp.js
@@ -6,8 +6,13 @@ var tour = [
     },
     {
         element: 'extended-customer-data',
-        title: 'Extended Customer Data ',
+        title: 'Extended Customer Data',
         description: 'Improve customer profiles by adding and grouping customer information using Custom Properties in the UI.',
+    },
+    {
+        element: 'pricing-table-licensing-model',
+        title: 'Pricing Table Licensing Model',
+        description: 'Easily maintain Pricing Table & Membership Plans for your SaaS products in the NetLicensing UI.',
     },
 ];
 
